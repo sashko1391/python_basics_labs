7b4b5f8a1e6249dfd40429f8e3763a447defb945
sashko1391 <sashko1391@gmail.com>
2025-11-25 23:54:58 +0000

    commit

---
+++ f_string.py
@@ -0,0 +1,14 @@
+name = "Sashko"
+surname="kravchenko"
+full_name=name+" "+surname
+print(full_name)
+
+name = "Sashko"
+surname="kravchenko"
+full_name=f"{name} {surname}"
+print(full_name.title())
+
+name = "Sashko"
+surname="kravchenko"
+full_name=f"{name} {surname}"
+print(f"Hello, {full_name.title()}")
