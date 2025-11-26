4f6efa4cda2b34d95eefd4804e26e0fab6abc7b4
sashko1391 <sashko1391@gmail.com>
2025-11-26 15:36:12 +0000

    commit

---
+++ lists.py
@@ -0,0 +1,14 @@
+names = ['Igor', 'Sasha', 'maks', 'Dimarik']
+print(names[0])
+
+print(names[2].title())
+
+print(names[-1].upper())    
+
+print(names[1], names[3])
+
+print(f"Hello, {names[2].capitalize()}, would you like to learn some Python today?")
+
+guests = ['buddha', 'jesus', 'muhammad', 'krishna', 'confucius']
+print(f"I would like to invite {guests[0].title()} to dinner.") 
+print(f"I would like to ask {guests[1].title()}, {guests[2].title()}, {guests[3].title()} and {guests[4].title()} to join us.")
---
+++ name_cases.py
@@ -0,0 +1,21 @@
+name="eric"
+name=name.title()
+message=f"Hello, {name}, would you like some coffee?"
+print(message)
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+famous_person="Borat"
+quote="I like!"
+message=f'{famous_person} once said: "{quote}"'
+print(message)
+
+nickname=" Sunny"
+filename="hindi.exe"
+message=f"Dear {nickname.strip()}, please run {filename.removesuffix(".exe")} and\n\t start with new line and tab."
+print(message)
+
+print(5+3)
+
